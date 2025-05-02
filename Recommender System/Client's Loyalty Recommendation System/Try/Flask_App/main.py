b22c41322cacd0be21f47ac725c41b93d3e23a69
DushyantGoyal1611 <goyaldushyant1611@gmail.com>
2025-05-02 16:19:24 +0530

    onfec

--- Recommender System/Client's Loyalty Recommendation System/Try/Flask_App/main.py
+++ Recommender System/Client's Loyalty Recommendation System/Try/Flask_App/main.py
@@ -191,6 +191,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    return render_template('intro.html')
+
+@app.route('/form')
+def form_page():
     return render_template('index.html')
 
 @app.route("/predict", methods=["POST"])
